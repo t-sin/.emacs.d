d2c483064f5cb00ac1cd62ccb3a01f06f8c5e920
t-sin <shinichi.tanaka45@gmail.com>
2014-09-08 21:31:16 +0900

    Deletes configurations no longer used

--- haskell.el
+++ haskell.el
@@ -3,10 +3,7 @@
 
 
 ;;;; haskell-mode
-(add-to-list 'load-path "~/opt/el-systems/haskell-mode/")
-
-(require 'haskell-mode)
-(require 'haskell-cabal)
+(auto-install-packages '(haskell-mode))
 
 ;; 拡張子とメジャーモード
 ;; lhs: Haskellで文芸的プログラミング
@@ -18,23 +15,11 @@
 (add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))     ;#!/usr/bin/env runghc 用
 (add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode)) ;#!/usr/bin/env runhaskell 用
 
+;; ドキュメンテーション
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
 ;; replの設定
 (setq haskell-program-name "/usr/bin/ghci")
-
-;; ghc-mod
-;; 
-;;  * 事前にcabal (Common Lispでいうquicklisp) でインストールしておく
-;;  * $ cabal update; cabal install cabal-install; cabal install ghc-mod
-;;
-;; cabal でインストールしたライブラリのコマンドが格納されている
-;; bin ディレクトリへのパスを exec-path に追加する
-(add-to-list 'exec-path (concat (getenv "HOME") "/.cabal/bin"))
-;; ghc-flymake.el などがあるディレクトリ ghc-mod を ~/.emacs.d 以下で管理することにした
-(add-to-list 'load-path "~/.cabal/share/ghc-mod-3.1.7") 
-
-(autoload 'ghc-init "ghc" nil t)
-
-(add-hook 'haskell-mode-hook
-  (lambda () (ghc-init)))
 
 
