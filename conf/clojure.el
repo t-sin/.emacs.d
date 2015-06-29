16b46532619a57229ddd9cc8584b5326576a7ddb
t-sin <shinichi.tanaka45@gmail.com>
2015-06-29 21:59:19 +0900

    Adds configurations about auto-complete

--- conf/clojure.el
+++ conf/clojure.el
@@ -6,13 +6,30 @@
 
 ;;; packages for package.el
 (defvar clojure-packages '(clojure-mode
-                           cider))
+                           cider
+                           ac-cider))
 
 (auto-install-packages clojure-packages)
 
-;; configure for clojure-mode
+;; configure clojure-mode
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
-;; configure for cider
+;; configure cider
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;; configure for auto-complete
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+  '(progn
+     (add-to-list 'ac-modes 'cider-mode)
+     (add-to-list 'ac-modes 'cider-repl-mode)))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
--- conf/common-lisp.el
+++ conf/common-lisp.el
@@ -2,10 +2,18 @@
 ;;;; please load this file in init.el
 
 
-;;;; SLIME(Common Lisp)の設定
-(add-to-list 'load-path "~/opt/el-systems/slime/")
+;;;; packages for package.el
+(defvar cl-packages '(ac-slime))
 
-(setq slime-net-coding-system 'utf-8-unix) ; 文字コード設定
+(auto-install-packages cl-packages)
+
+
+;;;; slime
+(add-to-list 'load-path "~/opt/el-systems/slime/")
+(require 'slime)
+
+;; 文字コード設定
+(setq slime-net-coding-system 'utf-8-unix)
 
 ;; Common Lisp 処理系の設定
 (setq slime-lisp-implementations
@@ -15,9 +23,21 @@
         (clisp ("~/.cim/bin/clisp"))
         (abcl ("~/.cim/bin/abcl"))))
 
-
-(require 'slime)
 (setq slime-contribs '(slime-fancy))
 (slime-setup)
 (slime-autodoc-mode) ; カーソル位置の関数情報をミニバッファに表示する
 
+
+;;;; hyperspec
+(setq common-lisp-hyperspec-root
+      (concat "file://" "/usr/share/doc/hyperspec/"))
+(setq common-lisp-hyperspec-symbol-table
+      (concat common-lisp-hyperspec-root
+              "Data/Map_Sym.txt"))
+
+
+;;;; auto-complete
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
