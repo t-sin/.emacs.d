9febcd7066e2317a98fd0c45da4a27e50e670168
t-sin <shinichi.tanaka45@gmail.com>
2015-09-07 20:43:53 +0900

    Remove jedi and some fix

--- conf/python.el
+++ conf/python.el
@@ -1,17 +1,12 @@
 ;;;; Python
 
 
-(defvar python-packages '(python-mode
-                          jedi))
+(defvar python-packages '(python-mode))
 (auto-install-packages python-packages)
 
+(autoload 'python-mode "python-mode" "python editting mode." t)
 (add-hook 'python-mode-hook
           '(lambda ()
              (setq indent-level 4)
              (setq python-indent 4)
              (setq tab-width 4)))
-
-(require 'jedi)
-(add-hook 'python-mode-hook 'jedi:setup)
-
-(setq jedi:complete-on-dot t)
