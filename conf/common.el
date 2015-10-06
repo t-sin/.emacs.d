957a71ad13dc89b8ed03f9b34e69eb6f7a8fa0e7
t-sin <shinichi.tanaka45@gmail.com>
2015-10-06 23:27:50 +0900

    Remove json-mode and swoop

--- conf/common.el
+++ conf/common.el
@@ -29,22 +29,3 @@
 
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
-;;; json-mode
-(auto-install-package 'json-mode)
-(autoload 'json-mode "json-mode"
-  "majort mode for editing json" t)
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
-
-;;;; swoop
-(auto-install-package 'swoop)
-(require 'swoop)
-
-(global-set-key (kbd "M-o") 'swoop)
-(global-set-key (kbd "C-M-o") 'swoop-multi)
-;; (global-set-key (kbd "M-o")   'swoop-pcre-regexp)
-(global-set-key (kbd "C-S-o") 'swoop-back-to-last-position)
- 
-(define-key isearch-mode-map (kbd "M-o") 'swoop-from-isearch)
-(define-key swoop-map (kbd "M-o") 'swoop-multi-from-swoop)
-   
-(setq swoop-font-size-change: nil)
