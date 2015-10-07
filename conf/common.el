5c5dcdc10636eca5fc8ffe4da9d38375a4b3f17e
t-sin <shinichi.tanaka45@gmail.com>
2015-10-07 22:43:14 +0900

    Enable json-mode again

--- conf/common.el
+++ conf/common.el
@@ -29,3 +29,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 
+;;; json-mode
+(auto-install-package 'json-mode)
+(autoload 'yaml-mode "json-mode"
+  "Major mode for editing JSON files" t)
+
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
