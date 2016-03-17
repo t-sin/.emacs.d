12954161e614a57f5e48bbd78cbe6aa04f45024f
t-sin <shinichi.tanaka45@gmail.com>
2016-03-18 01:16:51 +0900

    Move basic config

--- init.el
+++ init.el
@@ -1,55 +1,8 @@
 ;;;; emacs設定
 
 
-;;;; 基本設定
-
 ;; ロードパスの設定
 (add-to-list 'load-path "~/.emacs.d/conf")
-
-;; 言語を日本語に
-;(set-language-environment "Japanese")
-(set-language-environment "utf-8")
-
-;; 文字コードはできる限りutf8で
-;(prefer-coding-system 'utf-8)
-
-;; 起動時のメッセージ無効
-(setq inhibit-startup-message t)
-
-;; オートセーブ
-(auto-save-mode t)
-
-;; 終了時のオートセーブファイル削除
-;(setq delete-auto-save-files t)
-
-;; バックアップファイル
-;(setq backup-inhibited nil)
-
-;; Tabの代わりにスペースでインデント
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4 indent-tabs-mode nil)
-
-;; コピペ時、クリップボードにもつっこむ
-(cond (window-system
-	  (setq x-select-enable-clipboard t)))
-
-;; 選択範囲の挙動を他のエディタ風に
-;; （選択して文字列を入力すると、選択範囲が置き換わるとか）
-(cua-mode t)
-(setq cua-enable-cua-keys nil)
-
-;;;; キーバインドの割り当て
-;;;; 基本形: (define-key keymap keys definition)
-;;;; keymap: global-map, xxx-mode-map...
-;;;; keys: ex; C-x M-x a -> "\C-x\A-xa"
-;;;; definition: func
-
-;; C-hでbackspace
-(keyboard-translate ?\C-h ?\C-?)
-
-;; C-spaceでmark set
-;; AnthyのIM切り替えキーからCtrl-Spaceを消せばおk
-
 
 ;;;; package.el
 (require 'package)
