3b5e8ed596bdec81fd2cdc602519564ad62f3876
t-sin <shinichi.tanaka45@gmail.com>
2016-03-20 17:13:09 +0900

    Rearrange some configurations

--- conf/basic.el
+++ conf/basic.el
@@ -53,15 +53,3 @@
 (add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; yaml-mode
-(auto-install-package 'yaml-mode)
-(autoload 'yaml-mode "yaml-mode"
-  "Major mode for editing YAML files" t)
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-
-;; json-mode
-(auto-install-package 'json-mode)
-(autoload 'yaml-mode "json-mode"
-  "Major mode for editing JSON files" t)
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
