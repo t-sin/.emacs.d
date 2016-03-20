900a7aaa69124b94ea7a9fc241ea7427f0f7912c
t-sin <shinichi.tanaka45@gmail.com>
2016-03-20 16:37:48 +0900

    Cosmetic change

--- conf/haskell.el
+++ conf/haskell.el
@@ -1,25 +1,21 @@
-;;;; settings for Haskell
-;;;; please load this file in init.el
+;;;; config for Haskell
 
 
-;;;; haskell-mode
 (auto-install-packages '(haskell-mode))
 
-;; 拡張子とメジャーモード
-;; lhs: Haskellで文芸的プログラミング
+;; major modes for haskell files
+;; lhs: Literate Haskell; Literate Programming on Haskell
 (add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
 (add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
 (add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
 
-;; Haskell コマンドと haskell-mode
-(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))     ;#!/usr/bin/env runghc 用
-(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode)) ;#!/usr/bin/env runhaskell 用
+;; mapping Haskell REPL to haskell-mode
+(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
 
-;; ドキュメンテーション
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
-;; replの設定
 (setq haskell-program-name "/usr/bin/ghci")
 
 
--- conf/python.el
+++ conf/python.el
@@ -1,4 +1,4 @@
-;;;; Python
+;;;; python
 
 
 (defvar python-packages '(python-mode
