b5269842be023254cc0ae72bc063059355d9b81a
Shinichi Tanaka <sinichi.tanaka@iridge.jp>
2016-03-23 12:32:11 +0900

    Fix flycheck-pos-tip using way

--- conf/dev.el
+++ conf/dev.el
@@ -5,10 +5,9 @@
 (el-get-bundle flycheck)
 (el-get-bundle flycheck-pos-tip)
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'after-init-hook 'global-flycheck-mode)
 (eval-after-load 'flycheck
-  '(custom-set-variables
-    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+  (flycheck-pos-tip-mode))
 
 ;;; paredit-mode
 (el-get-bundle paredit)
